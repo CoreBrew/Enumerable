7e7e9b2108f67c8fff1fa91fbb3b000c061a1758
Jeppe Clausen <jeppe712@gmail.com>
2023-05-06 01:21:01 +0200

    test

---
+++ Src/CoreBrew.Enumerable/Enumerators/Locked/EnumeratorLockedReaderWriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace CoreBrew.Enumerable.Enumerators.Locked;
+
+public class EnumeratorLockedReaderWriter<T> : IEnumerator<T>
+{
+    private readonly ReaderWriterLock _readerWriterLock;
+    private readonly IEnumerator<T> _innerEnumerator;
+
+    public EnumeratorLockedReaderWriter(ReaderWriterLock readerWriterLock, IEnumerator<T> innerEnumerator)
+    {
+        _readerWriterLock = readerWriterLock;
+        _innerEnumerator = innerEnumerator;
+        _readerWriterLock.AcquireReaderLock(-1);
+    }
+
+    public bool MoveNext()
+    {
+        return _innerEnumerator.MoveNext();
+    }
+
+    public void Reset()
+    {
+        _innerEnumerator.Reset();
+    }
+    public T Current => _innerEnumerator.Current;
+
+    object? IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+        _readerWriterLock.ReleaseReaderLock();
+    }
+}
---
+++ Src/CoreBrew.Enumerable/Locked/EnumerableLockedReaderWriter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using CoreBrew.Enumerable.Enumerators.Locked;
+
+namespace CoreBrew.Enumerable.Locked;
+
+public class EnumerableLockedReaderWriter<T> : IEnumerable<T>
+{
+    private readonly IEnumerable<T> _innerEnumerable;
+    private readonly ReaderWriterLock _readerWriterLock;
+
+    public EnumerableLockedReaderWriter(IEnumerable<T> innerEnumerable, ReaderWriterLock readerWriterLock)
+    {
+        _innerEnumerable = innerEnumerable;
+        _readerWriterLock = readerWriterLock;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new EnumeratorLockedReaderWriter<T>(_readerWriterLock, _innerEnumerable.GetEnumerator());
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
